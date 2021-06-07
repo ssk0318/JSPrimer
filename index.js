dba11524be6a8c94f5b399afd8c2a2d6cfcf8e69
Sosuke Matsumoto <sskm0318@gmail.com>
2021-06-08 08:19:55 +0900

    値の評価と表示

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log(1);
+// 1
+
+console.log(1 + 1);
+// 2
+
+const total = 42 + 42;
+console.log(total);
+//84
+
+const value = "値";
+console.log(x);
+// xが定義されていないのでReferenceError
+
+console.log(value);
+// OK
